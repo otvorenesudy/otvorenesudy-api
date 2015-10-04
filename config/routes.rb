fe63c78a76564c68629d24707f8258df0ee6ab74
Samuel Molnar <molnar.samuel@gmail.com>
2015-10-04 13:18:24 +0200

    Add routes and allow only decrees to be queried

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :api do
+    constraints subdomain: 'api' do
+      resources :decrees, only: [] do
+        get :sync, on: :collection
+      end
+    end
+  end
 end
