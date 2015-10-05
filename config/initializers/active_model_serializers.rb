c0ff2e8e38d04c14fbf29354112d80d6f475a367
Samuel Molnar <molnar.samuel@gmail.com>
2015-10-05 15:25:11 +0200

    Add note for not using symbol as adapter

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,3 +1,4 @@
 # Place for ActiveModel::Serializer related settings
+# Make sure to use class, some tests use this config variable
 
 ActiveModel::Serializer.config.adapter = ActiveModel::Serializer::Adapter::Json
