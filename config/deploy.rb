9e64940e128e5fb99fbd546e0a15bc3ce7dfdbed
Samuel Molnar <molnar.samuel@gmail.com>
2015-10-07 16:26:26 +0200

    Add deploy config

---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+# Repository
+set :application, 'opencourts-api'
+set :scm,         :git
+set :repo_url,    'git@github.com:otvorenesudy/otvorenesudy-api.git'
+
+# RVM
+set :rvm_type,         :user
+set :rvm_ruby_version, '2.2.2'
+
+# Links
+set :linked_files, fetch(:linked_files, []).push('.env')
+set :linked_dirs,  fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+
+set :keep_releases, 5
+set :ssh_options, {
+  forward_agent: true
+}
+
+namespace :deploy do
+  after 'deploy:publishing', 'deploy:restart'
+  after 'finishing', 'deploy:cleanup'
+
+  desc 'Deploy app for first time'
+  task :cold do
+    invoke 'deploy:starting'
+    invoke 'deploy:started'
+    invoke 'deploy:updating'
+    invoke 'bundler:install'
+    invoke 'deploy:database' # This replaces deploy:migrations
+    invoke 'deploy:compile_assets'
+    invoke 'deploy:normalize_assets'
+    invoke 'deploy:publishing'
+    invoke 'deploy:published'
+    invoke 'deploy:finishing'
+    invoke 'deploy:finished'
+  end
+
+  desc 'Setup database'
+  task :database do
+    on roles(:db) do
+      within release_path do
+        with rails_env: (fetch(:rails_env) || fetch(:stage)) do
+           # TODO now we connect to existing DB
+        end
+      end
+    end
+  end
+
+  task :restart do
+    invoke 'unicorn:legacy_restart'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,7 @@
+set :stage,    :production
+set :branch,   'master'
+set :app_path, "#{fetch(:application)}-#{fetch(:stage)}"
+set :rails_env, :production
+set :deploy_to, "/home/deploy/projects/#{fetch(:app_path)}"
+
+server '195.146.144.210', user: 'deploy', roles: %w{app db web}
