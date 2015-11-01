99168a937c4053e245702ea172f48b1e71f6e4d6
Samuel Molnar <molnar.samuel@gmail.com>
2015-11-01 02:47:23 +0100

    Implement locales

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module OpenCourtsApi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :sk
 
+    # Load all locales
+    config.i18n.load_path += Dir[Rails.root.join 'config', 'locales', '**', '*.{rb,yml}']
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
