80c0ff01cadb1ee2e5691d9bfc24a3b6ed2b5b32
Samuel Molnar <molnar.samuel@gmail.com>
2015-11-02 18:30:01 +0100

    Bump version to 0.0.1.0

--- config/version.rb
+++ config/version.rb
@@ -2,9 +2,9 @@ module OpenCourtsApi
   module VERSION
     MAJOR = 0
     MINOR = 0
-    PATCH = 0
+    PATCH = 1
 
-    PRE = ''
+    PRE = '0'
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact * '.'
   end
