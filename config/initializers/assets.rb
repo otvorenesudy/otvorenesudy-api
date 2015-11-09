6992237b3f215035a82e29052ca316f72b431c50
Samuel Molnar <molnar.samuel@gmail.com>
2015-11-09 15:08:47 +0100

    Properly compile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( fontawesome-webfont.woff fontawesome-webfont.woff2 fontawesome-webfont.ttf )
+Rails.application.config.assets.precompile += %w( application.css application.js )
