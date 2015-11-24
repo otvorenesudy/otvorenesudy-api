6308cfb0f8ebce9fe96e077da008bb6e40bb77e7
Samuel Molnar <molnar.samuel@gmail.com>
2015-11-24 21:50:18 +0100

    Use VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,14 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'rubygems'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr'
+  config.hook_into :webmock # or :fakeweb
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
