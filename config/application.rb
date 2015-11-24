2826bbc3d4866835d889ef21fd6966b68f98e657
Samuel Molnar <molnar.samuel@gmail.com>
2015-11-24 21:50:03 +0100

    Activate ActiveJob

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'active_model/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
-# require 'active_job/railtie'
+require 'active_job/railtie'
 # require 'action_mailer/railtie'
 require 'sprockets/railtie'
 # require 'rails/test_unit/railtie'
@@ -20,6 +20,11 @@ module OpenCourtsApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Autoload paths
+    config.autoload_paths += %W(
+      #{config.root}/lib
+    )
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
