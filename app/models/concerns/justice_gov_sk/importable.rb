b55cfba5453c1502dfb092aee171ca424c5f5b54
Samuel Molnar <molnar.samuel@gmail.com>
2015-11-26 12:25:34 +0100

    Implement Importable concern

--- app/models/concerns/justice_gov_sk/importable.rb
+++ app/models/concerns/justice_gov_sk/importable.rb
@@ -1,7 +1,21 @@
 module JusticeGovSk
   module Importable
+    def importable_restricted_attributes_for_update
+      [:html]
+    end
+
     def import_from(attributes)
-      # TODO
+      record = find_by(uri: attributes[:uri])
+
+      return create!(attributes) unless record
+
+      record.assign_attributes(attributes.except(*importable_restricted_attributes_for_update))
+
+      return unless record.changed?
+
+      record.assign_attributes(attributes.slice(*importable_restricted_attributes_for_update))
+
+      record.save!
     end
   end
 end
---
+++ spec/models/justice_gov_sk/decree_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+require 'models/concerns/justice_gov_sk/importable_spec'
+
+RSpec.describe JusticeGovSk::Decree do
+  it_behaves_like JusticeGovSk::Importable do
+    let(:attributes) {
+      {
+        uri: 'https://obcan.justice.sk/infosud/-/infosud/i-detail/rozhodnutie/305e5973-67a3-4258-a19e-b1dd31f2d095%3A9e90c187-fe0c-4957-a0d2-5f508d6ade04',
+        forma: 'Trestný rozkaz',
+        sud: 'Okresný súd Dunajská Streda',
+        sud_uri: 'https://obcan.justice.sk/infosud/-/infosud/detail/sud/111',
+        sudca: 'JUDr. Mária Képessyová',
+        sudca_uri: 'https://obcan.justice.sk/infosud/-/infosud/detail/sudca/666',
+        datum_vydania_rozhodnutia: '21.11.2015',
+        spisova_znacka: '0T/167/2015',
+        identifikacne_cislo_spisu: '2215000304',
+        oblast_pravnej_upravy: 'Poriadok vo verejných veciach - Trestné právo',
+        povaha_rozhodnutia: 'Prvostupňové nenapadnuté opravnými prostriedkami',
+        ecli: 'ECLI:SK:OSDS:2015:2215000304.1',
+        predpisy: [
+          '/SK/ZZ/2005/300/#paragraf-37',
+          '/SK/ZZ/2005/300/#paragraf-38.odsek-2',
+          '/SK/ZZ/2005/300',
+          '/SK/ZZ/2005/300/#paragraf-61.odsek-2',
+          '/SK/ZZ/2005/300/#paragraf-50.odsek-1',
+          '/SK/ZZ/2005/300/#paragraf-36',
+          '/SK/ZZ/2005/300/#paragraf-348.odsek-1',
+          '/SK/ZZ/2005/300/#paragraf-61.odsek-1',
+          '/SK/ZZ/2005/300/#paragraf-49.odsek-1.pismeno-a'
+        ],
+        pdf_uri: 'https://obcan.justice.sk/content/public/item/9e90c187-fe0c-4957-a0d2-5f508d6ade04',
+        html: '<html></html>'
+      }
+    }
+
+    let(:updated_attributes) { attributes.merge(forma: 'Rozhodnutie', sud: 'Okresný súd Prievidza') }
+  end
+end
