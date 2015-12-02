95329652fa2a399964c7f8eb27a051ec9adb6fd6
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-02 18:24:40 +0100

    Implement autoloading

--- lib/justice_gov_sk.rb
+++ lib/justice_gov_sk.rb
@@ -1,15 +1,11 @@
-require 'active_support/core_ext/object/blank'
+require 'active_support/all'
 require 'active_job'
 require 'nokogiri'
 
 module JusticeGovSk
-  require 'justice_gov_sk/downloader'
-  require 'justice_gov_sk/list_crawler'
-  require 'justice_gov_sk/resource_crawler'
-  require 'justice_gov_sk/infrastructure'
-
-  require 'justice_gov_sk/decrees/list_crawler'
-  require 'justice_gov_sk/decrees/resource_parser'
-  require 'justice_gov_sk/decrees/resource_crawler'
-  require 'justice_gov_sk/decrees'
+  autoload :Downloader
+  autoload :ListCrawler
+  autoload :ResourceCrawler
+  autoload :Infrastructure
+  autoload :Decrees
 end
