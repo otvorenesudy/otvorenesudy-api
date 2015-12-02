d005ad33dffa48d28248878ef5377cb2a9fc7012
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-02 20:22:45 +0100

    Fix refactoring typo

--- spec/support/satisfy_matcher.rb
+++ spec/support/satisfy_matcher.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define :satisfy do |*matchers|
   match do |actual|
-    compound = matchers[1..-1].inject(matchers[0]).each { |c, matcher| c.and(matcher) }
+    compound = matchers[1..-1].inject(matchers[0]) { |c, matcher| c.and(matcher) }
 
     expect(actual).to compound
   end
