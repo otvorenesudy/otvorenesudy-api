099f0c47cc30376b2d5d9c672e09b68ba23f25f4
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-07 22:49:30 +0100

    Simplify Importable concern a bit

--- app/models/concerns/justice_gov_sk/importable.rb
+++ app/models/concerns/justice_gov_sk/importable.rb
@@ -5,16 +5,13 @@ module JusticeGovSk
     end
 
     def import_from(attributes)
-      record = find_by(uri: attributes[:uri])
-
-      return create!(attributes) unless record
+      record = find_or_initialize_by(uri: attributes[:uri])
 
       record.assign_attributes(attributes.except(*importable_restricted_attributes_for_update))
 
       return unless record.changed?
 
       record.assign_attributes(attributes.slice(*importable_restricted_attributes_for_update))
-
       record.save!
     end
   end
