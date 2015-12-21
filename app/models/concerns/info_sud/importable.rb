756885f91b0a1bd34f9c340b2a0291807736dd71
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-21 19:27:50 +0100

    Fix Importer class

--- app/models/concerns/info_sud/importable.rb
+++ app/models/concerns/info_sud/importable.rb
@@ -3,7 +3,7 @@ module InfoSud
     def import_from(attributes)
       record = find_or_initialize_by(guid: attributes[:guid])
 
-      Importer.import_or_update(record, attributes: attributes)
+      ::Importer.import_or_update(record, attributes: attributes)
     end
   end
 end
