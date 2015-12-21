4139a6c3f938ef9672d9895d63c8f12f6d7f3ff2
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-21 21:04:07 +0100

    Use example json

--- spec/lib/info_sud/parser_spec.rb
+++ spec/lib/info_sud/parser_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 require 'info_sud'
 
 RSpec.describe InfoSud::Parser do
-  let(:content) { fixture('info_sud/courts.json').read }
+  let(:content) { fixture('info_sud/example.json').read }
 
   describe '.parse' do
     it 'parses json records' do
       records = InfoSud::Parser.parse(content)
 
       expect(records.size).to eql(2)
-      expect(records.first[:guid]).to eql('sud_101')
+      expect(records.first[:title]).to eql('Example')
     end
   end
 end
