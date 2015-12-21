13d93df4f71b62175bbc22e90295c155a8a89d8e
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-21 19:49:19 +0100

    Use top-level namespace

--- app/models/concerns/obcan_justice_sk/importable.rb
+++ app/models/concerns/obcan_justice_sk/importable.rb
@@ -3,7 +3,7 @@ module ObcanJusticeSk
     def import_from(attributes)
       record = find_or_initialize_by(uri: attributes[:uri])
 
-      Importer.import_or_update(record, attributes: attributes, restricted_attributes: [:html])
+      ::Importer.import_or_update(record, attributes: attributes, restricted_attributes: [:html])
     end
   end
 end
