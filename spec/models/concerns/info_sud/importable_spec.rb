ec0558c093d7339092c570229ae76fcc182314ef
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-29 18:56:59 +0100

    Break dependencies of concerns

--- spec/models/concerns/info_sud/importable_spec.rb
+++ spec/models/concerns/info_sud/importable_spec.rb
@@ -1,38 +1,63 @@
 require 'rails_helper'
-require 'models/concerns/importable_spec'
 
 RSpec.shared_examples_for InfoSud::Importable do
-  it_behaves_like 'Importable' do
-    let(:record) { described_class.find_by(guid: attributes[:guid]) }
-    let(:restricted_attributes_for_update) { [] }
-
-    # TODO add more descriptive attributes for each model later
-    let(:attributes) {
-      {
-        guid: '1',
-        data: {
-          guid: '1',
-          category: {
-            attribute: [
-              id: '1',
-              name: 'Example'
-            ]
-          }
-        }
+  let(:record) { described_class.find_by(guid: attributes[:guid]) }
+  let(:attributes) {
+    {
+      guid: '1',
+      category: {
+        attribute: [
+          id: '1',
+          name: 'Example'
+        ]
       }
     }
+  }
 
-    let(:updated_attributes) { attributes.merge!(
-      data: {
-        guid: '1',
-        category: {
-          attribute: [
-            id: '2',
-            name: 'Example'
-          ]
-        }
-      }
-    )
+  let(:updated_attributes) { attributes.merge!(
+    category: {
+      attribute: [
+        id: '2',
+        name: 'Example'
+      ]
     }
+  )}
+
+  describe '.import_from' do
+    it 'imports record by attributes' do
+      described_class.import_from(attributes)
+
+      record_attributes = record.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+      expect(record_attributes.deep_symbolize_keys).to eql(guid: attributes[:guid], data: attributes)
+    end
+
+    context 'when record is already imported' do
+      it 'updates record' do
+        described_class.import_from(attributes)
+        described_class.import_from(updated_attributes)
+
+        record_attributes = record.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+        expect(record_attributes.deep_symbolize_keys).to eql(guid: updated_attributes[:guid], data: attributes)
+      end
+
+      context 'without any change' do
+        it 'does not update record' do
+          time = 30.minutes.ago
+
+          Timecop.freeze(time) do
+            described_class.import_from(attributes)
+          end
+
+          described_class.import_from(attributes)
+
+          record_attributes = record.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+          expect(record.updated_at.change(usec: 0)).to eq(time.change(usec: 0))
+          expect(record_attributes.deep_symbolize_keys).to eql(guid: attributes[:guid], data: attributes)
+        end
+      end
+    end
   end
 end
--- spec/models/concerns/obcan_justice_sk/importable_spec.rb
+++ spec/models/concerns/obcan_justice_sk/importable_spec.rb
@@ -1,9 +1,67 @@
 require 'rails_helper'
-require 'models/concerns/importable_spec'
 
 RSpec.shared_examples_for ObcanJusticeSk::Importable do
-  it_behaves_like 'Importable' do
-    let(:record) { described_class.find_by(uri: attributes[:uri]) }
-    let(:restricted_attributes_for_update) { [:html] }
+  let(:record) { described_class.find_by(uri: attributes[:uri]) }
+  let(:restricted_attributes_for_update) { [:html] }
+
+  describe '.import_from' do
+    it 'imports record by attributes' do
+      described_class.import_from(attributes)
+
+      record_attributes = record.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+      expect(record_attributes.deep_symbolize_keys).to eql(attributes)
+    end
+
+    context 'when record is already imported' do
+      it 'updates record' do
+        described_class.import_from(attributes)
+        described_class.import_from(updated_attributes)
+
+        record_attributes = record.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+        expect(record_attributes.deep_symbolize_keys).to eql(updated_attributes)
+      end
+
+      context 'without any change' do
+        it 'does not update record' do
+          time = 30.minutes.ago
+
+          Timecop.freeze(time) do
+            described_class.import_from(attributes)
+          end
+
+          described_class.import_from(attributes)
+
+          record_attributes = record.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+          expect(record.updated_at.change(usec: 0)).to eq(time.change(usec: 0))
+          expect(record_attributes.deep_symbolize_keys).to eql(attributes)
+        end
+      end
+
+      context 'when restricted attributes for update changes' do
+        it 'does not update record' do
+          time = 30.minutes.ago
+
+          Timecop.freeze(time) do
+            described_class.import_from(attributes)
+          end
+
+          restricted_attributes = restricted_attributes_for_update.inject(Hash.new) do |hash, attribute|
+            hash[attribute] = rand.to_s
+
+            hash
+          end
+
+          described_class.import_from(attributes.merge(restricted_attributes))
+
+          record_attributes = record.attributes.symbolize_keys.except(:id, :created_at, :updated_at)
+
+          expect(record.updated_at.change(usec: 0)).to eq(time.change(usec: 0))
+          expect(record_attributes.deep_symbolize_keys).to eql(attributes)
+        end
+      end
+    end
   end
 end
