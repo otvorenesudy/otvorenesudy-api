fe263310e734a39916d4fa7269c0cef735661113
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-29 19:10:07 +0100

    Fix spec

--- spec/lib/info_sud_spec.rb
+++ spec/lib/info_sud_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe InfoSud do
 
     it 'imports data from url to repository' do
       allow(downloader).to receive(:download_file).with('url') { 'path/to/file' }
-      allow(extractor).to receive(:extract).with('path/to/file').and_yield('data 1', 'data 2')
+      allow(extractor).to receive(:extract).with('path/to/file').and_yield('data 1').and_yield('data 2')
 
       expect(importer).to receive(:import).with('data 1', repository: repository)
       expect(importer).to receive(:import).with('data 2', repository: repository)
