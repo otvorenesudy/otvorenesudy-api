cf70328f73b66f7f25fc0a5f6b9ffb533c43eebd
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-29 18:55:44 +0100

    Handle attributes mapping in model

--- app/models/concerns/info_sud/importable.rb
+++ app/models/concerns/info_sud/importable.rb
@@ -4,7 +4,7 @@ module InfoSud
       record = find_or_initialize_by(guid: attributes[:guid])
       dispatcher = UpdateDispatcher.new(record)
 
-      ::Importer.import_or_update(dispatcher, attributes: attributes)
+      ::Importer.import_or_update(dispatcher, attributes: { guid: attributes[:guid], data: attributes })
     end
   end
 end
