7e58a7efaf0ebeb9fec63447a284f1bd488c1978
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-30 13:40:30 +0100

    Make Extractor example more thorough

--- spec/lib/info_sud/extractor_spec.rb
+++ spec/lib/info_sud/extractor_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe InfoSud::Extractor do
-  let(:archive) { fixture('info_sud/reg-sudy_json.zip') }
+  let(:archive) { fixture('info_sud/example.zip') }
 
   describe '.extract' do
     it 'extracts and yields all files from zip archive' do
       expect { |block|
         InfoSud::Extractor.extract(archive.path, &block)
-      }.to yield_control.once
+      }.to yield_successive_args("Example File #1\n", "Example File #2\n")
     end
   end
 end
