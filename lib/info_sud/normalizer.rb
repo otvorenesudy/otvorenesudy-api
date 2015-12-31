19596cc90820adb39f4493408c0d7136a56afa2d
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-31 13:25:03 +0100

    Import legacy normalizer

---
+++ lib/info_sud/normalizer.rb
@@ -0,0 +1,45 @@
+module InfoSud
+  class Normalizer
+    module Methods
+      using UnicodeString
+
+      def normalize_phone(value)
+        value = value.gsub(/(\d+\s*)+/) { |part|
+          part.gsub!(/\s/, '')
+
+          case part.size
+          when  7 then "#{part[0   ]} #{part[1..3]} #{part[4..6]} "
+          when  8 then "#{part[0..1]} #{part[2..4]} #{part[5..7]} "
+          when 10 then "#{part[0..3]} #{part[4..6]} #{part[7..9]} "
+          else
+            part + ' '
+          end
+        }
+
+        value.gsub!(/\s*\/+\s*/, '/')
+        value.gsub!(/\s*\-+\s*/, ' - ')
+        value.gsub!(/\s*([\,\;])+\s*/, ', ')
+
+        value.gsub!(/fax\s*\.\s*/i, ' fax ')
+        value.gsub!(/kl(apka)?\s*\.\s*/i, ' klapka ')
+
+        value.gsub!(/\s*@\s*/, '@')
+
+        value.strip.squeeze(' ')
+      end
+
+      def normalize_hours(value)
+        value = value.gsub(/[a-z]+/i, '')
+
+        times = value.split(/\s*\-\s*|\,\s*|\;\s*|\s+/).map do |time|
+          hour, minute = time.split(/\:/)
+          "#{'%d' % hour.to_i}:#{'%02d' % minute.to_i}"
+        end
+
+        times.each_slice(2).map { |interval| "#{interval.first} - #{interval.last}" }.join ', '
+      end
+    end
+
+    extend Methods
+  end
+end
