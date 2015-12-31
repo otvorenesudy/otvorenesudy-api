3ed459c9710cf0ffbe4c6e4d3a1b182b9d550374
Samuel Molnar <molnar.samuel@gmail.com>
2015-12-31 13:38:59 +0100

    Remove redundant specs

--- spec/features/obcan_justice_sk/crawl_selection_procedures_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'ObcanJusticeSk SelectionProcedures' do
-  scenario 'crawls list of selection procedures', vcr: { cassette_name: 'obcan_justice_sk/crawl_selection_procedure_list' } do
-    pending
-    fail
-  end
-end
