5a968e394ca7eeb6b510ea48910f225c4a42c081
Samuel Molnar <molnar.samuel@gmail.com>
2016-01-03 12:09:44 +0100

    Import Legacy normalizations

---
+++ lib/legacy.rb
@@ -0,0 +1,4 @@
+module Legacy
+  require 'legacy/string'
+  require 'legacy/normalizer'
+end
--- lib/unicode_string.rb
+++ lib/unicode_string.rb
@@ -1,21 +1,5 @@
 module UnicodeString
-  module UnicodeMethods
-    extend ActiveSupport::Concern
-
-    included do
-      alias :utf8 :mb_chars
-    end
-
-    def ascii
-      return self if self.ascii_only?
-
-      self.utf8.normalize(:kd).bytes.map { |b| (0x00..0x7F).include?(b) ? b.chr : '' }.join
-    end
-  end
-
   refine String do
-    include UnicodeMethods
-
     def strip
       dup.tap { |string| string.strip! }
     end
@@ -26,9 +10,5 @@ module UnicodeString
 
       left || right ? self : nil
     end
-  end
-
-  class UnicodedString < ::String
-    include UnicodeMethods
   end
 end
