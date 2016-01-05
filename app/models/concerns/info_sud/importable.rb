6e02e6d5d2266f38421e72223c8e4f0c0ad94ff6
Samuel Molnar <molnar.samuel@gmail.com>
2016-01-05 23:28:54 +0100

    Simplify update logic for models

--- app/models/concerns/info_sud/importable.rb
+++ app/models/concerns/info_sud/importable.rb
@@ -2,10 +2,9 @@ module InfoSud
   module Importable
     def import_from(attributes)
       record = find_or_initialize_by(guid: attributes[:guid])
-      delegator = UpdateDelegator.new(record)
       attributes = attributes.slice(:guid).merge(data: attributes)
 
-      ::Importer.import_or_update(delegator, attributes: attributes)
+      ImportManager.import_or_update(record, attributes: attributes)
     end
   end
 end
---
+++ spec/services/import_manager_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'active_support/all'
+require_relative '../../app/services/import_manager'
+
+RSpec.describe ImportManager do
+  let(:record) { double(:record, attributes: { name: 'Peter', surname: 'Smith'} ) }
+  let(:attributes) { { name: 'Peter', surname: 'Parker' } }
+
+  describe '.import_or_update' do
+    context 'when attributes change' do
+      it 'updates record with attributes' do
+        expect(record).to receive(:update_attributes!).with(attributes)
+
+        ImportManager.import_or_update(record, attributes: attributes)
+      end
+    end
+
+    context 'without change' do
+      let(:attributes) { { name: 'Peter', surname: 'Smith' } }
+
+      it 'does not dispatch update for attributes' do
+        expect(record).not_to receive(:update_attributes!)
+
+        ImportManager.import_or_update(record, attributes: attributes)
+      end
+    end
+
+    context 'when restricted attributes for update changes' do
+      it 'does not update record' do
+        expect(record).not_to receive(:update_attributes!)
+
+        ImportManager.import_or_update(record, attributes: attributes, restricted_attributes_for_update: [:surname])
+      end
+    end
+  end
+end
