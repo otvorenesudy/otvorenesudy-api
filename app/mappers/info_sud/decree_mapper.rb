8f7517b0abd92442501c444194a730f414060646
Samuel Molnar <molnar.samuel@gmail.com>
2016-01-14 21:45:55 +0100

    Add InfoSud::DecreeMapper

---
+++ app/mappers/info_sud/decree_mapper.rb
@@ -0,0 +1,82 @@
+module InfoSud
+  class DecreeMapper
+    FORM_CODE_MAP = {
+      'Rozsudok' => 'A',
+      'Uznesenie' => 'N',
+      'Opravné uznesenie' => 'R',
+      'Dopĺňací rozsudok' => 'D',
+      'Platobný rozkaz' => 'P',
+      'Zmenkový platobný rozkaz' => 'M',
+      'Európsky platobný rozkaz' => 'E',
+      'Šekový platobný rozkaz' => 'S',
+      'Rozkaz na plnenie' => 'L',
+      'Rozsudok pre zmeškanie' => 'K',
+      'Rozsudok pre uznanie' => 'U',
+      'Rozsudok bez odôvodnenia' => 'F',
+      'Uznesenie bez odôvodnenia' => 'C',
+      'Osvedčenie' => 'B',
+      'Trestný rozkaz' => 'T'
+    }
+
+    def initialize(data)
+      @data = data.deep_symbolize_keys
+    end
+
+    def uri
+      "https://obcan.justice.sk/infosud/-/infosud/i-detail/rozhodnutie/#{@data[:guid]}"
+    end
+
+    def ecli
+      @data[:ecli].presence
+    end
+
+    def court
+      InfoSud::Normalizer.normalize_court_name(@data[:sud_nazov])
+    end
+
+    def judges
+      Array.wrap(@data[:sudca_meno].presence).map do |name|
+        InfoSud::Normalizer.normalize_person_name(name)
+      end
+    end
+
+    def date
+      Time.parse(@data[:datum_vydania_rozhodnutia]) if @data[:datum_vydania_rozhodnutia].present?
+    end
+
+    def file_number
+      @data[:identifikacne_cislo_spisu].presence
+    end
+
+    def case_number
+      @data[:spisova_znacka].presence
+    end
+
+    def form
+      @data[:forma_rozhodnutia].presence
+    end
+
+    def form_code
+      # TODO redundant, remove from schema
+      FORM_CODE_MAP[form] if form
+    end
+
+    def natures
+      Array.wrap(@data[:povaha_rozhodnutia].presence)
+    end
+
+    def legislation_area
+      Array.wrap(@data[:oblast_pravnej_upravy].presence).first
+    end
+
+    def legislation_subarea
+      Array.wrap(@data[:podoblast_pravnej_upravy].presence).first
+    end
+
+    def legislations
+      @data[:odkazovane_predpisy].map do |string|
+        InfoSud::Normalizer.partition_legislation(string).merge(value: string, value_unprocessed: string)
+      end
+    end
+  end
+end
---
+++ spec/mappers/info_sud/decree_mapper_spec.rb
@@ -0,0 +1,123 @@
+require 'spec_helper'
+require 'info_sud'
+require_relative '../../../app/mappers/info_sud/decree_mapper'
+
+RSpec.describe InfoSud::DecreeMapper do
+  subject { InfoSud::DecreeMapper.new(data) }
+
+  let(:data) {
+    {
+      "ecli" => "ECLI:SK:OSMT:2015:5714206355.1",
+      "guid" => "99947314-4390-4668-833a-d92ccb476b6b:da85930e-5742-4e33-83d7-7d2f44282b37",
+      "sud_typ" => "Okresný súd",
+      "sud_guid" => "135",
+      "sud_kraj" => "Žilinský kraj",
+      "sud_nazov" => "Okresný súd Martin",
+      "sud_okres" => "Okres Martin",
+      "sudca_guid" => "136",
+      "sudca_meno" => "JUDr. Miriam Štillová",
+      "dokument_nazov" => "Rozhodnutie_15C-113-2014.pdf",
+      "spisova_znacka" => "15C/113/2014",
+      "index_timestamp" => "2015-12-02T19:39:33.121Z",
+      "sudca_meno_text" => "JUDr. Miriam Štillová",
+      "vazby_na_eurlex" => "",
+      "dokument_fulltext" => "Fulltext",
+      "forma_rozhodnutia" => "Rozhodnutie",
+      "nadpis_rozhodnutia" => "",
+      "povaha_rozhodnutia" => [
+        "Prvostupňové nenapadnuté opravnými prostriedkami"
+      ],
+      "odkazovane_predpisy" => [
+        "/SK/ZZ/2005/36",
+        "/SK/ZZ/2005/36/#paragraf-62"
+      ],
+      "oblast_pravnej_upravy" => [
+        "Rodinné právo"
+      ],
+      "dokument_download_link" => "https://obcan.justice.sk/content/public/item/da85930e-5742-4e33-83d7-7d2f44282b37",
+      "odkazovane_predpisy_iri" => "",
+      "podoblast_pravnej_upravy" => [
+        "Vyživovacie povinnosti"
+      ],
+      "datum_vydania_rozhodnutia" => "2015-03-08T23:00:00Z",
+      "identifikacne_cislo_spisu" => "5714206355",
+      "vazby_na_eu_sudne_rozhodnutie" => "",
+      "vazby_na_sk_sudne_rozhodnutie" => ""
+    }
+  }
+
+  describe '#ecli' do
+    it 'maps ecli' do
+      expect(subject.ecli).to eql('ECLI:SK:OSMT:2015:5714206355.1')
+    end
+  end
+
+  describe '#uri' do
+    it 'maps uri' do
+      expect(subject.uri).to eql('https://obcan.justice.sk/infosud/-/infosud/i-detail/rozhodnutie/99947314-4390-4668-833a-d92ccb476b6b:da85930e-5742-4e33-83d7-7d2f44282b37')
+    end
+  end
+
+  describe '#court' do
+    it 'maps court name' do
+      expect(subject.court).to eql('Okresný súd Martin')
+    end
+  end
+
+  describe '#judges' do
+    it 'maps judges names' do
+      expect(subject.judges).to eql(['JUDr. Miriam Štillová'])
+    end
+  end
+
+  describe '#case_number' do
+    it 'maps case number' do
+      expect(subject.case_number).to eql('15C/113/2014')
+    end
+  end
+
+  describe '#file_number' do
+    it 'maps file number' do
+      expect(subject.file_number).to eql('5714206355')
+    end
+  end
+
+  describe '#date' do
+    it 'maps decree date' do
+      expect(subject.date).to eql(Time.parse('2015-03-08T23:00:00Z'))
+    end
+  end
+
+  describe '#form' do
+    it 'maps decree form' do
+      expect(subject.form).to eql('Rozhodnutie')
+    end
+  end
+
+  describe '#natures' do
+    it 'maps decree natures' do
+      expect(subject.natures).to eql(['Prvostupňové nenapadnuté opravnými prostriedkami'])
+    end
+  end
+
+  describe '#legislations' do
+    it 'maps legislations' do
+      expect(subject.legislations).to eql([
+        { year: 2005, number: 36, value: '/SK/ZZ/2005/36', value_unprocessed: '/SK/ZZ/2005/36' },
+        { year: 2005, number: 36, paragraph: '62', value: '/SK/ZZ/2005/36/#paragraf-62', value_unprocessed: '/SK/ZZ/2005/36/#paragraf-62' }
+      ])
+    end
+  end
+
+  describe '#legislation_area' do
+    it 'maps legislation area' do
+      expect(subject.legislation_area).to eql('Rodinné právo')
+    end
+  end
+
+  describe '#legislation_subarea' do
+    it 'maps legislatio subarea' do
+      expect(subject.legislation_subarea).to eql('Vyživovacie povinnosti')
+    end
+  end
+end
