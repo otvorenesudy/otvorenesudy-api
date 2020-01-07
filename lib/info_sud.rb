09ff1b8fc2d0ff19051142031b672a3a6e1925a9
Samuel Molnar <molnar.samuel@gmail.com>
2020-01-07 10:26:39 +0100

    Fix links

--- lib/info_sud.rb
+++ lib/info_sud.rb
@@ -13,15 +13,6 @@ module InfoSud
   COURTS_URL = 'https://obcan.justice.sk/opendata?p_p_id=isuopendata_WAR_isufront&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=reg-sudy_json&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2'
   JUDGES_URL = 'https://obcan.justice.sk/opendata?p_p_id=isuopendata_WAR_isufront&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=reg-sudcovia_json.zip&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2'
   HEARINGS_URL = 'https://obcan.justice.sk/opendata?p_p_id=isuopendata_WAR_isufront&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=isu_sp_json.zip&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2'
-  DECREE_URLS = [
-    'https://obcan.justice.sk/opendata?p_p_id=isuopendata_WAR_isufront&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=isu_sr_json.zip&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2',
-    *[2016, 2017, 2018].map { |year|
-      "https://obcan.justice.sk/opendata?p_p_id=isuopendata_WAR_isufront&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=isu_sr_#{year}_json.zip&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2"
-    },
-    *(1..12).map { |n|
-      "https://obcan.justice.sk/opendata?p_p_id=isuopendata_WAR_isufront&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=isu_sr_2019-#{n}_json.zip&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=2"
-    }
-  ]
 
   def self.import_courts
     path = InfoSud::Downloader.download_file(COURTS_URL)
@@ -30,6 +21,10 @@ module InfoSud
   end
 
   def self.import_judges
+    urls = Nokogiri::HTML(Curl.get('https://obcan.justice.sk/opendata').body_str).css('a').map { |e|
+      e['href']
+    }.select { |e| e.match(/p_p_resource_id=isu_sr.+_json.zip/) }
+
     path = InfoSud::Downloader.download_file(JUDGES_URL)
 
     import(path, repository: InfoSud::Judge)
