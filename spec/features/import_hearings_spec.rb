f1906cc35f7a0641985eedc16b004f6ca39a5731
Samuel Molnar <molnar.samuel@gmail.com>
2020-06-20 12:36:26 +0200

    Fix specs

--- spec/features/import_hearings_spec.rb
+++ spec/features/import_hearings_spec.rb
@@ -25,45 +25,45 @@ RSpec.feature 'Import Hearings' do
 
       Timecop.freeze(created_at) do
         InfoSud::Importer.import(data, repository: InfoSud::Hearing)
+
+        expect(InfoSud::Hearing.count).to eql(2)
+        expect(Hearing.count).to eql(2)
+
+        hearing = Hearing.find_by(uri: 'https://obcan.justice.sk/infosud/-/infosud/i-detail/pojednavanie/94fed4fd-af4a-42c3-8c21-788358207927')
+        court = Court.find_by(name: 'Okresný súd Košice I')
+        type = Hearing::Type.find_by(value: 'Trestné')
+        section = Hearing::Section.find_by(value: 'T')
+        subject = Hearing::Subject.find_by(value: '4Pv 461/14 - § 189 ods. 1 Tr. zák.')
+        form = Hearing::Form.find_by(value: 'Hlavné pojednávanie s rozhodnutím')
+        proceeding = Proceeding.find_by(file_number: '7115010154')
+
+        expect(hearing.attributes.symbolize_keys.except(:id, :created_at, :updated_at)).to eql(
+          uri: 'https://obcan.justice.sk/infosud/-/infosud/i-detail/pojednavanie/94fed4fd-af4a-42c3-8c21-788358207927',
+          source_id: source.id,
+          case_number: '3T/9/2015',
+          file_number: '7115010154',
+          date: Time.parse('2015-12-07T12:30:00Z').in_time_zone,
+          room: '135',
+          commencement_date: nil,
+          selfjudge: true,
+          note: nil,
+          anonymized_at: created_at,
+          special_type: nil,
+
+          court_id: court.id,
+          hearing_type_id: type.id,
+          hearing_section_id: section.id,
+          hearing_subject_id: subject.id,
+          hearing_form_id: form.id,
+          proceeding_id: proceeding.id
+        )
+
+        expect(hearing.proposers.to_a).to be_empty
+        expect(hearing.opponents.to_a).to be_empty
+        expect(hearing.defendants.count).to eql(1)
+        expect(hearing.defendants.pluck(:name).first).to match(/\A\w\. \w\.\z/)
+        expect(hearing.judges.to_a).to eql([judges[0]])
       end
-
-      expect(InfoSud::Hearing.count).to eql(2)
-      expect(Hearing.count).to eql(2)
-
-      hearing = Hearing.find_by(uri: 'https://obcan.justice.sk/infosud/-/infosud/i-detail/pojednavanie/94fed4fd-af4a-42c3-8c21-788358207927')
-      court = Court.find_by(name: 'Okresný súd Košice I')
-      type = Hearing::Type.find_by(value: 'Trestné')
-      section = Hearing::Section.find_by(value: 'T')
-      subject = Hearing::Subject.find_by(value: '4Pv 461/14 - § 189 ods. 1 Tr. zák.')
-      form = Hearing::Form.find_by(value: 'Hlavné pojednávanie s rozhodnutím')
-      proceeding = Proceeding.find_by(file_number: '7115010154')
-
-      expect(hearing.attributes.symbolize_keys.except(:id, :created_at, :updated_at)).to eql(
-        uri: 'https://obcan.justice.sk/infosud/-/infosud/i-detail/pojednavanie/94fed4fd-af4a-42c3-8c21-788358207927',
-        source_id: source.id,
-        case_number: '3T/9/2015',
-        file_number: '7115010154',
-        date: Time.parse('2015-12-07T12:30:00Z').in_time_zone,
-        room: '135',
-        commencement_date: nil,
-        selfjudge: true,
-        note: nil,
-        anonymized_at: created_at,
-        special_type: nil,
-
-        court_id: court.id,
-        hearing_type_id: type.id,
-        hearing_section_id: section.id,
-        hearing_subject_id: subject.id,
-        hearing_form_id: form.id,
-        proceeding_id: proceeding.id
-      )
-
-      expect(hearing.proposers.to_a).to be_empty
-      expect(hearing.opponents.to_a).to be_empty
-      expect(hearing.defendants.count).to eql(1)
-      expect(hearing.defendants.pluck(:name).first).to match(/\A\w\. \w\.\z/)
-      expect(hearing.judges.to_a).to eql([judges[0]])
     end
 
     scenario 'updates hearings' do
@@ -75,49 +75,49 @@ RSpec.feature 'Import Hearings' do
 
       Timecop.freeze(updated_at) do
         InfoSud::Importer.import(updated_data, repository: InfoSud::Hearing)
+
+        hearings = Hearing.where('updated_at >= ?', updated_at)
+
+        expect(hearings.size).to eql(1)
+
+        hearing = hearings.first
+        court = Court.find_by(name: 'Okresný súd Nitra')
+        type = Hearing::Type.find_by(value: 'Civilné')
+        section = Hearing::Section.find_by(value: 'C')
+        subject = Hearing::Subject.find_by(value: '4Pv 461/14 - § 189 ods. 1 Tr. zák.')
+        form = Hearing::Form.find_by(value: 'Hlavné pojednávanie s rozhodnutím')
+        proceeding = Proceeding.find_by(file_number: '123456789')
+
+        expect(hearing.attributes.symbolize_keys.except(:id, :created_at, :updated_at)).to eql(
+          uri: 'https://obcan.justice.sk/infosud/-/infosud/i-detail/pojednavanie/94fed4fd-af4a-42c3-8c21-788358207927',
+          source_id: source.id,
+          case_number: '3T/9/2015',
+          file_number: '123456789',
+          date: Time.parse('2015-12-07T12:30:00Z').in_time_zone,
+          room: '135',
+          commencement_date: nil,
+          selfjudge: true,
+          note: nil,
+          anonymized_at: updated_at,
+          special_type: nil,
+
+          court_id: court.id,
+          hearing_type_id: type.id,
+          hearing_section_id: section.id,
+          hearing_subject_id: subject.id,
+          hearing_form_id: form.id,
+          proceeding_id: proceeding.id
+        )
+
+        expect(hearing.proposers.count).to eql(1)
+        expect(hearing.proposers.pluck(:name).first).to match(/\A\w\. \w\.\z/)
+
+        expect(hearing.opponents.count).to eql(1)
+        expect(hearing.opponents.pluck(:name).first).to match(/\A\w\. \w\.\z/)
+
+        expect(hearing.defendants.to_a).to be_empty
+        expect(hearing.judges.to_a).to eql([judges[1]])
       end
-
-      hearings = Hearing.where('updated_at >= ?', updated_at)
-
-      expect(hearings.size).to eql(1)
-
-      hearing = hearings.first
-      court = Court.find_by(name: 'Okresný súd Nitra')
-      type = Hearing::Type.find_by(value: 'Civilné')
-      section = Hearing::Section.find_by(value: 'C')
-      subject = Hearing::Subject.find_by(value: '4Pv 461/14 - § 189 ods. 1 Tr. zák.')
-      form = Hearing::Form.find_by(value: 'Hlavné pojednávanie s rozhodnutím')
-      proceeding = Proceeding.find_by(file_number: '123456789')
-
-      expect(hearing.attributes.symbolize_keys.except(:id, :created_at, :updated_at)).to eql(
-        uri: 'https://obcan.justice.sk/infosud/-/infosud/i-detail/pojednavanie/94fed4fd-af4a-42c3-8c21-788358207927',
-        source_id: source.id,
-        case_number: '3T/9/2015',
-        file_number: '123456789',
-        date: Time.parse('2015-12-07T12:30:00Z').in_time_zone,
-        room: '135',
-        commencement_date: nil,
-        selfjudge: true,
-        note: nil,
-        anonymized_at: updated_at,
-        special_type: nil,
-
-        court_id: court.id,
-        hearing_type_id: type.id,
-        hearing_section_id: section.id,
-        hearing_subject_id: subject.id,
-        hearing_form_id: form.id,
-        proceeding_id: proceeding.id
-      )
-
-      expect(hearing.proposers.count).to eql(1)
-      expect(hearing.proposers.pluck(:name).first).to match(/\A\w\. \w\.\z/)
-
-      expect(hearing.opponents.count).to eql(1)
-      expect(hearing.opponents.pluck(:name).first).to match(/\A\w\. \w\.\z/)
-
-      expect(hearing.defendants.to_a).to be_empty
-      expect(hearing.judges.to_a).to eql([judges[1]])
     end
   end
 end
