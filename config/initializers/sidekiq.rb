8f524c4b3de4deeb32efe94541501dd4dfb07877
Samuel Molnar <molnar.samuel@gmail.com>
2020-11-07 12:27:26 +0100

    Specify Redis database number

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,13 @@
 require 'sidekiq/web'
 
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://localhost:6379/1' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://localhost:6379/1' }
+end
+
 Sidekiq::Web.use(Rack::Auth::Basic) do |_, password|
   password == ENV['OPENCOURTS_API_SIDEKIQ_PASSWORD']
 end
