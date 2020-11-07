499d1c1fc55804c018530355e71296f0d7b931ea
Samuel Molnar <molnar.samuel@gmail.com>
2020-11-07 13:00:50 +0100

    Fix UpdateNotifier

--- app/services/update_notifier.rb
+++ app/services/update_notifier.rb
@@ -2,7 +2,9 @@ class UpdateNotifier
   def self.notify(record)
     # TODO provide Rest API instead of shared queue later
 
-    Sidekiq::Client.push(
+    client = Sidekiq::Client.new(ConnectionPool.new { Redis.new(url: 'redis://localhost:6379/0') })
+
+    client.push(
       'class' => 'UpdateRepositoryJob',
       'queue' => 'probe',
       'args' => [record.class.name, record.id]
